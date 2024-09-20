5ce99e6cd0a272ab2033b820cab4b6c2510ef6d1
Suvarchala04 <162489811+Suvarchala04@users.noreply.github.com>
2024-09-20 20:24:59 +0530

    Time: 223 ms (8.9%), Space: 46.5 MB (5.99%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,8 +1,19 @@
 class Solution {
+    public boolean linearSearch(int idx,int n,int[] nums){
+        for(int i=0;i<nums.length;i++){
+            if(i!=idx && nums[i]==n){
+                return true;
+            }
+        }
+        return false;
+    }
     public int singleNumber(int[] nums) {
-        int res=0;
-        for(int num:nums){
-            res ^= num;
+        int res=-1;
+        for(int i=0;i<nums.length;i++){
+            if(!linearSearch(i,nums[i],nums)){
+                res=nums[i];
+                break;
+            }
         }
         return res;
     }
